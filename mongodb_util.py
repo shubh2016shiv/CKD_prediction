52f840d405d4570787e1d3eef2ed9ff9045fc64f
Shubham Singh <shubh2014shiv@gmail.com>
2022-10-18 03:07:47 +0530

    Update mongodb_util.py

--- mongodb_util.py
+++ mongodb_util.py
@@ -1,25 +1,30 @@
 import pymongo
 import pandas as pd
+from secure_db_connection import SecureConnect
 
 
 class MongoDB:
     def __init__(self, database_name, connection_string):
-        self.database_name = database_name
-        self.connection_string = connection_string
+        self._database_name = database_name
+        self._connection_string = connection_string
+        secure_connection = SecureConnect()
+        self._password = secure_connection.decrypt()
+        self._user_name = secure_connection.get_user_name()
+        self._connection_string = self._connection_string.replace("<username>",self._user_name).replace("<password>",self._password)
         self.client = None
 
     def connect_to_mongo_db(self):
         try:
             if self.client is None:
-                self.client = pymongo.MongoClient(self.connection_string)
+                self.client = pymongo.MongoClient(self._connection_string)
                 print("MongoDB connected")
         except (Exception,) as e:
             print("Error in connecting MongoDB Cloud")
 
     def get_collection(self):
         if self.client is not None:
-            database = self.client[self.database_name]
-            collection = database.ckd_collection
+            database = self.client[self._database_name]
+            collection = database.ckd_collection  # Collection Name in the Database
             return collection
         else:
             print("Cannot connect with database due to failure in Internet connection")
