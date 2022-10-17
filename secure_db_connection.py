dc9a2ee7116961ae1d95d63ebd17cd85e9501e24
Shubham Singh <shubh2014shiv@gmail.com>
2022-10-18 04:00:06 +0530

    Update secure_db_connection.py

--- secure_db_connection.py
+++ secure_db_connection.py
@@ -3,10 +3,11 @@ from cryptography.fernet import Fernet
 
 
 class SecureConnect:
-    def __init__(self, password=None):
+    def __init__(self, password=None, username=None):
         config = ConfigParser()
         config.read('config.ini')
         self.password = password
+        self.username = username
         self._key = config['mongo']['key']
         self._user_name = config['mongo']['user_name']
         self._encrypted_password = config['mongo']['encrypted_password']
@@ -20,7 +21,7 @@ class SecureConnect:
         return self._user_name
 
     def is_connection_success(self):
-        if self.password == self.decrypt():
+        if (self.password == self.decrypt()) and (self.username == self._user_name):
             return True
         else:
             return False
