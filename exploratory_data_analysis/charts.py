876d86d43d01db88d6c2ea247590da32666744b4
Shubham Singh <shubh2014shiv@gmail.com>
2022-10-19 03:38:52 +0530

    Update charts.py

--- exploratory_data_analysis/charts.py
+++ exploratory_data_analysis/charts.py
@@ -9,10 +9,61 @@ from exploratory_data_analysis.descriptive_analysis import DescriptiveStatistics
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
+def get_proper_column_name(col_name):
+    if col_name == 'bp':
+        return 'Blood Pressure'
+    elif col_name == 'sg':
+        return 'Specific Gravity'
+    elif col_name == 'al':
+        return 'Albumin'
+    elif col_name == 'su':
+        return 'Sugar'
+    elif col_name == 'rbc':
+        return 'Red Blood Cells'
+    elif col_name == 'pc':
+        return 'Pus Cell'
+    elif col_name == 'pcc':
+        return 'Pus Cell Clumps'
+    elif col_name == 'ba':
+        return 'Bacteria'
+    elif col_name == 'bgr':
+        return 'Blood Glucose Random'
+    elif col_name == 'bu':
+        return 'Blood Urea'
+    elif col_name == 'sc':
+        return 'Serum Creatinine'
+    elif col_name == 'sod':
+        return 'Sodium'
+    elif col_name == 'pot':
+        return 'Potassium'
+    elif col_name == 'hemo':
+        return 'Hemoglobin'
+    elif col_name == 'pcv':
+        return 'Packed Cell Volume'
+    elif col_name == 'wc':
+        return 'White Blood Cell Count'
+    elif col_name == 'rc':
+        return 'Red Blood Cell Count'
+    elif col_name == 'htn':
+        return 'Hypertension'
+    elif col_name == 'dm':
+        return 'Diabetes Mellitus'
+    elif col_name == 'cad':
+        return 'Coronary Artery Disease'
+    elif col_name == 'appet':
+        return 'Appetite'
+    elif col_name == 'pe':
+        return 'Pedal Edema'
+    elif col_name == 'ane':
+        return 'Anemia'
+    else:
+        return 'classification (CKD or No-CKD)'
+
+
 class Visualization:
     def __init__(self, data: pd.core.frame.DataFrame):
         self.data = data.copy(deep=True)
-        self.data['classification'] = self.data['classification'].apply(lambda x: x.replace("\t",""))
+        self.data['classification'] = self.data['classification'].apply(lambda x: x.replace("\t", ""))
         self.data['pcv'] = self.data['pcv'].replace(['\t?'], np.nan).replace(['\t43'], '43')
         self.data['pcv'] = self.data['pcv'].apply(lambda x: float(x))
 
@@ -41,16 +92,18 @@ class Visualization:
                         break
 
                     if color is None:
-                        fig = px.histogram(self.data, x=columns[i])
+                        fig = px.histogram(self.data, x=columns[i],
+                                           title="Histogram for '{}'".format(get_proper_column_name(columns[i])))
                         j.plotly_chart(fig, use_container_width=True)
                     else:
-                        fig = px.histogram(self.data, x=columns[i],color=color)
+                        fig = px.histogram(self.data, x=columns[i], color=color,
+                                           title="Histogram for '{}'".format(get_proper_column_name(columns[i])))
                         j.plotly_chart(fig, use_container_width=True)
 
                     i += 1
 
         if 'classification' in columns:
-            plot_histogram_as_subplots(columns,color='classification')
+            plot_histogram_as_subplots(columns, color='classification')
         else:
             plot_histogram_as_subplots(columns)
 
@@ -65,6 +118,13 @@ class Visualization:
         return (round(len(outliers) / len(self.data) * 100, 3), round(len(inliers) / len(self.data) * 100, 3))
 
     def show_box_plot(self, columns):
+        show_categories = st.checkbox(
+            "Tick the checkbox to show Box plots separately for each category of CKD and non-CKD.")
+        if show_categories:
+            classification_color = "classification"
+        else:
+            classification_color = None
+
         if set(columns).issubset(set(self.numerical_cols)):
             i = 0
             while i < len(columns):
@@ -74,7 +134,10 @@ class Visualization:
                     if i >= len(columns):
                         break
 
-                    fig = px.box(self.data, y=columns[i], points="all", notched=False)
+                    fig = px.box(self.data, y=columns[i], points="all", notched=False, color=classification_color,
+                                 color_discrete_map={"ckd": "red",
+                                                     "notckd": "green"},
+                                 title="Box Plot for Data - {}".format(get_proper_column_name(columns[i])))
                     j.plotly_chart(fig, use_container_width=True)
                     with j:
                         perc_inliers, perc_outliers = self.calculateOutliersInliers(columns[i])
@@ -95,7 +158,8 @@ class Visualization:
                     if i >= len(columns):
                         break
 
-                    fig = px.violin(self.data, y=columns[i], points="all", box=True)
+                    fig = px.violin(self.data, y=columns[i], points="all", box=True,
+                                    title="Violin Plot for '{}'".format(get_proper_column_name(columns[i])))
                     j.plotly_chart(fig, use_container_width=True)
                     i += 1
         else:
@@ -103,37 +167,11 @@ class Visualization:
                 "Categorical columns are not allowed for Violin Plots. Remove the categorical columns : %s" % str(
                     self.categorical_cols))
 
-    def show_pie_plot(self, columns):
-
-        aggregation_option = st.selectbox("Choose the type of Aggregation", options=['Average', 'Max', 'Min', 'Count'])
-        if aggregation_option != 'Count':
-            numerical_var_agg_option = st.multiselect("Numerical Variables", options=self.numerical_cols)
-            group_by_option = st.selectbox("Group By Categorical Column", options=self.categorical_cols)
-
-            def perform_aggregation(numerical_var_agg_option, group_by_option):
-                if numerical_var_agg_option == "Average":
-                    return self.data.groupby([group_by_option]).mean().reset_index
-                elif numerical_var_agg_option == "Max":
-                    return self.data.groupby([group_by_option]).max().reset_index
-                elif numerical_var_agg_option == "Min":
-                    return self.data.groupby([group_by_option]).min().reset_index
-
-            i = 0
-            while i < len(columns):
-                c1, c2 = st.columns(2)
-                for j in [c1, c2]:
-
-                    if i >= len(columns):
-                        break
-
-                    fig = px.pie(perform_aggregation(), values=columns[i], names='Gender')
-                    j.plotly_chart(fig, use_container_width=True)
-                    i += 1
-
-    def calculate_null_values_perc(self,columns):
+    def calculate_null_values_perc(self, columns):
         null_values_df = pd.DataFrame(self.data[columns].isna().sum()).reset_index()
         null_values_df.columns = ['Column', 'Number of Null Values']
-        null_values_df['Null Values Percent'] = (null_values_df['Number of Null Values'] / self.data.isna().sum().sum()) * 100
+        null_values_df['Null Values Percent'] = (null_values_df[
+                                                     'Number of Null Values'] / self.data.isna().sum().sum()) * 100
         null_values_df.sort_values(by='Null Values Percent', ascending=True, inplace=True)
         return null_values_df
 
@@ -141,32 +179,55 @@ class Visualization:
         if operation == 'Distributions':
             self.show_histogram(columns)
         if operation == "Missing Values":
+            st.info("The white horizontal patches indicates that value is missing at specific row number.")
             fig = plt.figure()
             msno.matrix(self.data, figsize=(25, 5), fontsize=12)
             st.pyplot()
 
             null_values_df = self.calculate_null_values_perc(columns=columns)
-            fig = px.bar(data_frame=null_values_df,x='Null Values Percent',y='Column',orientation='h',color='Null Values Percent')
+            fig = px.bar(data_frame=null_values_df, x='Null Values Percent', y='Column', orientation='h',
+                         color='Null Values Percent',
+                         hover_name='Number of Null Values',
+                         title="Null Values per Column")
             st.plotly_chart(fig, use_container_width=True)
 
-        if operation == "Bubble Chart":
+        if operation == "Scatter Plot":
+            st.write('''The scatter diagram graphs pairs of numerical data, with one variable on each axis, to look for a relationship between them.
+             If the variables are correlated, the points will fall along a line or curve. 
+             The better the correlation, the tighter the points will hug the line.''')
+            show_trend_line = st.checkbox("Tick the checkbox to show Trend Line(s).")
+            if show_trend_line:
+                trendline = "lowess"
+            else:
+                trendline = None
+
             if len(columns) == 2 and set(columns).issubset(set(self.numerical_cols)):
-                third_dim = st.selectbox(label="Select the Third Dimension (Numerical)",
-                                         options=['None',
-                                                  'Aspartate_Aminotransferase',
-                                                  'Alamine_Aminotransferase',
-                                                  'Alkaline_Phosphotase'])
+
                 fig = px.scatter(self.data, x=columns[0], y=columns[1],
-                                 size=None if third_dim == "None" else third_dim,
-                                 color="Gender",
-                                 hover_name="Dataset", size_max=60, height=1000)
+                                 color="classification",
+                                 size_max=60, height=1000,
+                                 trendline=trendline,
+                                 color_discrete_map={"ckd": "red",
+                                                     "notckd": "green"},
+                                 marginal_x="histogram", marginal_y="rug",
+                                 symbol="classification",
+                                 title="Scatter Plot between '{}' and '{}'".format(get_proper_column_name(columns[0]),
+                                                                              get_proper_column_name(columns[1])))
                 st.plotly_chart(fig, use_container_width=True)
             elif len(columns) > 2:
-                st.warning("Only two numerical columns are allowed for Bubble chart")
+                st.warning("Only two numerical columns are allowed for Scatter plot")
             elif not set(columns).issubset(set(self.numerical_cols)):
                 st.warning("Selected column should only be Numerical. Categorical columns are not allowed")
         if operation == "Box Plots":
+            st.write('''
+            A box plot is a statistical representation of the distribution of a variable through its quartiles. 
+            The ends of the box represent the lower and upper quartiles, while the median (second quartile) is marked by a line inside the box.
+            Box plot is also used for visualizing the potential outliers.
+            When reviewing a box plot, an outlier is defined as a data point that is located outside the whiskers of the box plot.
+            ''')
             self.show_box_plot(columns)
 
         if operation == "Violin Plots":
+            st.write('''A violin plot is a statistical representation of numerical data. 
+            It is similar to a box plot, with the addition of a rotated kernel density plot on each side.''')
             self.show_violin_plot(columns)
