ee38446c9b1ffbc79a83c8833d08d87558345c62
Shubham Singh <shubh2014shiv@gmail.com>
2022-10-21 04:03:57 +0530

    Update feature_selection.py

--- ml_steps/feature_selection.py
+++ ml_steps/feature_selection.py
@@ -11,11 +11,19 @@ from sklearn.model_selection import cross_validate
 
 
 def load_pickle_file(pickle_name):
+    """
+    Loading the serialized pickle file
+    :return: Object
+    """
     with open(pickle_name, 'rb') as f:
         return pickle.load(f)
 
 
 def save_as_pickle_file(obj, pickle_file_name):
+    """
+    Serialize an Object in pickle format
+    :return: None
+    """
     with open(pickle_file_name, 'wb') as f:
         pickle.dump(obj, f)
 
@@ -30,6 +38,10 @@ class FeatureSelection:
         #self.categorical_columns = [column for column in self.categorical_columns if column!='classification']
 
     def anova_test_feature_scores(self):
+        """
+        Perform ANOVA testing to visually identify the relationship strength between numerical feature variables and categorical target variable
+        :return: ploty figure
+        """
         X = self.X[self.numerical_columns]
         fs = SelectKBest(score_func=f_classif, k=len(X.columns))
 
@@ -49,6 +61,10 @@ class FeatureSelection:
         return fig
 
     def chi_square_test_feature_scores(self):
+        """
+        Perform Chi-Square testing to visually identify the relationship strength between categorical feature variables and categorical target variable
+        :return: ploty figure
+        """
         X = self.X[[column for column in self.categorical_columns if column != 'classification']]
         fs = SelectKBest(score_func=chi2, k=len(X.columns))
 
@@ -78,6 +94,10 @@ class FeatureSelection:
         return fig
 
     def plot_feature_importance(self,importance, names, model_type):
+        """
+        Identity the important feature based on Random Forest model
+        :return: plotly figure
+        """
         # Create arrays from feature importance and feature names
         feature_importance = np.array(importance)
         feature_names = np.array(names)
@@ -97,12 +117,20 @@ class FeatureSelection:
         return fig
 
     def analyse_feature_importance(self):
+        """
+        Analysing the importance of each feature variable on target variable
+        :return: plotly figure
+        """
         model = RandomForestClassifier(random_state=100)
         model.fit(self.X, self.y)
         fig = self.plot_feature_importance(model.feature_importances_,self.X.columns,'RANDOM FOREST')
         return fig
 
     def select_features(self):
+        """
+        Selecting 9 best numerical features (based on ANOVA Testing) and 5 best categorical features (based on Chi-Square Testing)
+        :return: list of selected columns
+        """
         # For selecting 9 Best Numerical Features
         anova_k_best = SelectKBest(score_func=f_classif, k=9)
         # For selecting 5 Best Categorical Features
@@ -121,6 +149,10 @@ class FeatureSelection:
         return selected_num_cols + selected_cat_cols
 
     def compare_performance_before_after_feat_sel(self):
+        """
+        Comparing the performance before and after feature selection to check if the decision to select certain features was right
+        :return: pandas dataframe with performace comparison results
+        """
         # Before feature selection
         X = self.X.copy(deep=True)
         y = self.y.copy(deep=True)
@@ -150,6 +182,10 @@ class FeatureSelection:
         return cv_results
 
     def perform_feature_selection(self,X: pd.core.frame.DataFrame):
+        """
+        Perform the feature selection
+        :return: Pandas Dataframe with selected feature variables
+        """
         selected_features = self.select_features()
         save_as_pickle_file(obj=selected_features,pickle_file_name='resources/processed_data/selected_features.pkl')
         feature_selected_df = X[self.select_features()]
